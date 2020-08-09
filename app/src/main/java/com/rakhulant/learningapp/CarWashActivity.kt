a5c62db83125b82a97ff658bc147d5ab10734d34
Rakhulant <rakrah51@gmail.com>
2020-08-09 12:40:12 +0530

    changes

---
+++ app/src/main/java/com/rakhulant/learningapp/CarWashActivity.kt
@@ -0,0 +1,39 @@
+package com.rakhulant.learningapp
+
+import android.app.DatePickerDialog
+import android.app.TimePickerDialog
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.TimePicker
+import kotlinx.android.synthetic.main.activity_car_wash.*
+import java.util.*
+
+class CarWashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_car_wash)
+    }
+    fun datePicker(view: View){
+        val calned = Calendar.getInstance()
+        val year = calned.get(Calendar.YEAR)
+        val month = calned.get(Calendar.MONTH)
+        val day = calned.get(Calendar.DAY_OF_MONTH)
+        DatePickerDialog(this, DatePickerDialog.OnDateSetListener { datePicker, i, i2, i3 ->
+            val date = "$i3/$i2/$i"
+            btnDate.text = date
+        }, year, month, day).show()
+
+    }
+    fun timePicker(view: View){
+        val calned = Calendar.getInstance()
+        val hr:Int = calned.get(Calendar.HOUR)
+        val min = calned.get(Calendar.MINUTE)
+        TimePickerDialog(
+            this,
+            TimePickerDialog.OnTimeSetListener { timePicker: TimePicker?, i: Int, i2: Int ->
+                val time = "$i:$i2"
+                btnTime.text = time
+            }, hr, min, false).show()
+    }
+}
